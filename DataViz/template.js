c3f11d0e41a46cf93c9f1be085098c44eb36d4da
Manxueying Li <manxueying.li@columbia.edu>
2023-10-09 11:24:51 -0400

    update jsooooooon

---
+++ DataViz/template.js
@@ -0,0 +1,19 @@
+const originalData =
+
+
+
+// Transform the data
+const transformedData = {
+    nodes: originalData.nodes.map(node => ({
+        id: node.id,
+        name: node.name,
+        val: node.val
+    })),
+    links: originalData.links.map(link => ({
+        source: link.source,
+        target: link.target
+    }))
+};
+
+// Export the transformed data so it can be used in another script
+window.transformedData = transformedData;
